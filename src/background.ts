163758e395e36b3116e8f4853fd15e6c5aee6368
Magnus Skog <magnus.m.skog@gmail.com>
2024-07-17 21:36:07 +0200

    Redirect support

--- src/background.ts
+++ src/background.ts
@@ -1,16 +1,33 @@
 export {}
 
-chrome.runtime.onInstalled.addListener(function () {
-  interface Action {
-    url: string
-    name: string
-    parameter: string
-    context: chrome.contextMenus.ContextType
-  }
+enum ActionType {
+  Redirect = "Redirect",
+  Background = "Background"
+}
 
+interface Action {
+  url: string
+  name: string
+  parameter: string
+  context: chrome.contextMenus.ContextType
+  type: ActionType
+}
+
+chrome.runtime.onInstalled.addListener(function () {
   chrome.storage.sync.get({ url: "" }, (result) => {
     fetch(result.url)
       .then((response) => response.json())
+      .then((data: any[]) => {
+        return data.map(
+          (item): Action => ({
+            url: item.url,
+            name: item.name,
+            parameter: item.parameter,
+            context: item.context,
+            type: (item.type as ActionType) || ActionType.Background
+          })
+        )
+      })
       .then((actions: Action[]) => {
         actions.forEach((action) => {
           chrome.contextMenus.create({
@@ -21,12 +38,18 @@ chrome.runtime.onInstalled.addListener(function () {
         })
 
         chrome.contextMenus.onClicked.addListener((event) => {
-          const action = actions.find((action) => {
-            return action.url == event.menuItemId
-          })
+          const action = actions.find(
+            (action) => action.url === event.menuItemId
+          )
 
           if (action) {
-            fetch(`${action.url}?${action.parameter}=${event.pageUrl}`)
+            if (action.type === ActionType.Redirect) {
+              chrome.tabs.create({
+                url: `${action.url}?${action.parameter}=${event.pageUrl}`
+              })
+            } else {
+              fetch(`${action.url}?${action.parameter}=${event.pageUrl}`)
+            }
           }
         })
       })
