99622ee5948925f27a87d9d0ec17a4d64858796d
Magnus Skog <magnus.m.skog@gmail.com>
2024-10-04 20:54:18 +0200

    fix: update notification icon URL to use a secure link

--- src/background.ts
+++ src/background.ts
@@ -80,7 +80,7 @@ function setupContextMenuListener() {
 
         chrome.notifications.create({
           type: "basic",
-          iconUrl: "/assets/icon.png",
+          iconUrl: "https://bellard.org/bpg/2.png",
           title: "Action Executed",
           message: `The action "${action.name}" was executed successfully.`
         })
